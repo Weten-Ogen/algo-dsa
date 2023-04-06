0244987b2e1479243a29f8aad8b9908daae4185b
Weten-Ogen <alphaoware@gmail.com>
2023-04-06 02:44:54 +0000

    practice on linkedlist

--- lec/revision/linkedQ.py
+++ lec/revision/linkedQ.py
@@ -1,1 +1,23 @@
 # Reverse a linked list 
+from linked import SingleList
+
+c = SingleList()
+c.add_list([1,2,3,4,5])
+
+def reverse_list(c):
+    prev = None
+    curr = c.head
+    while curr is not None:
+        temp = 
+        print(curr.data)
+       
+        curr = temp
+    
+    
+
+reverse_list(c)
+
+
+
+""" c.print_list()
+ """
